35de1f4b2052de3c7886d702c16d1d9b57446bea
LibXZR <i@xzr.moe>
2022-01-28 20:37:32 +0800

    Move PackageManager new user logic after dispatching
    The dispatchUserAdded function is doing something like sending broadcast
    to notify system services to be ready for the new user, which is of
    great significance. And the PackageManager is somehow calling
    SettingsProvider via the following call chain.
    
    at android.provider.Settings$Secure.getStringForUser(Settings.java:5851)
    at com.android.server.voiceinteraction.VoiceInteractionManagerService$VoiceInteractionManagerServiceStub.initForUserNoTracing(VoiceInteractionManagerService.java:420)
    at com.android.server.voiceinteraction.VoiceInteractionManagerService$VoiceInteractionManagerServiceStub.initForUser(VoiceInteractionManagerService.java:412)
    at com.android.server.voiceinteraction.VoiceInteractionManagerService$1.getPackages(VoiceInteractionManagerService.java:156)
    at com.android.server.pm.permission.DefaultPermissionGrantPolicy.grantDefaultSystemHandlerPermissions(DefaultPermissionGrantPolicy.java:529)
    at com.android.server.pm.permission.DefaultPermissionGrantPolicy.grantDefaultPermissions(DefaultPermissionGrantPolicy.java:378)
    at com.android.server.pm.permission.LegacyPermissionManagerService$Internal.grantDefaultPermissions(LegacyPermissionManagerService.java:385)
    at com.android.server.pm.PackageManagerService.onNewUserCreated(PackageManagerService.java:26626)
    
    But unfortunately, SettingsProvider relies on OverlayManagerService to
    correctly setup overlay for config_navBarInteractionMode. And the
    OverlayManagerService only get initialized for the new user after receiving
    the broadcast mentioned above, which means the overlay can never be
    set up. This is leading to broken navigation bar and gestures after
    switching to the new user when config_navBarInteractionMode is set to 2.
    
    Change-Id: Iba3f7b68b4e8055f2dcd93a4cb07c773d8bf0747
    Signed-off-by: LibXZR <i@xzr.moe>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/pm/UserManagerService.java
+++ services/core/java/com/android/server/pm/UserManagerService.java
@@ -4970,9 +4970,6 @@ public class UserManagerService extends IUserManager.Stub {
                 mBaseUserRestrictions.updateRestrictions(userId, restrictions);
             }
 
-            t.traceBegin("PM.onNewUserCreated-" + userId);
-            mPm.onNewUserCreated(userId, /* convertedFromPreCreated= */ false);
-            t.traceEnd();
             applyDefaultUserSettings(userTypeDetails, userId);
             setDefaultCrossProfileIntentFilters(userId, userTypeDetails, restrictions, parentId);
 
@@ -4991,6 +4988,10 @@ public class UserManagerService extends IUserManager.Stub {
             } else {
                 dispatchUserAdded(userInfo, token);
             }
+
+            t.traceBegin("PM.onNewUserCreated-" + userId);
+            mPm.onNewUserCreated(userId, /* convertedFromPreCreated= */ false);
+            t.traceEnd();
 
         } finally {
             Binder.restoreCallingIdentity(ident);
