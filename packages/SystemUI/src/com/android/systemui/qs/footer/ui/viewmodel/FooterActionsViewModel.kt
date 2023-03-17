262cb96e6e4a9ef286230608922f484ea3b38996
John Galt <johngaltfirstrun@gmail.com>
2023-03-17 13:26:59 -0400

    SystemUI/footer: fix settings icon tint for dual tone
    Footer actions were rewritten as part of QPR2 merge.

--- packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
+++ packages/SystemUI/src/com/android/systemui/qs/footer/ui/viewmodel/FooterActionsViewModel.kt
@@ -149,7 +149,11 @@ class FooterActionsViewModel(
                 R.drawable.ic_settings,
                 ContentDescription.Resource(R.string.accessibility_quick_settings_settings)
             ),
-            iconTint = null,
+            iconTint =
+                Utils.getColorAttrDefaultColor(
+                    context,
+                    com.android.internal.R.attr.textColorPrimary,
+                ),
             backgroundColor = R.attr.offStateColor,
             this::onSettingsButtonClicked,
             this::onSettingsButtonLongClicked
