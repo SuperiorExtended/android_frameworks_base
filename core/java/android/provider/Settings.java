73f12d1c753d64289723cb64870181ef909dbd7b
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-12 12:54:47 +0800

    Volume Dialog: Add switch for media controller button [1/2]
    Change-Id: I58293a4bb5312e713d8919a1efa6fabc53146b51

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6379,6 +6379,11 @@ public final class Settings {
          * @hide
          */
         public static final String TRANSPARENT_POWER_DIALOG_DIM = "transparent_power_dialog_dim";
+        
+        /**
+         * @hide
+         */
+        public static final String VOLUME_MEDIA_OUTPUT_TOGGLE = "volume_media_output_toggle";
 
         /**
          * Whether StatusBar icons should use app icon
